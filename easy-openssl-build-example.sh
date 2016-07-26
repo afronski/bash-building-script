55d8dea9e397dccc2cb015e4d241d27dd1d38e70
Christophe Sonntag <Christophe@wlcl.net>
2016-07-26 05:35:29 +0200

    add openssl example for MSYS2

---
+++ easy-openssl-build-example.sh
@@ -0,0 +1,38 @@
+﻿#! /bin/bash
+
+# Little script to build openssl
+# Author : Christophe Sonntag (https://github.com/c-sonntag)
+
+
+# Add MINGW to path if is not set
+#export PATH=${PATH}:/r/my-mingw-folder/bin
+. set-gcc32
+
+# Set the system for the ./config script, 
+# use "mingw64" for x64 build
+export SYSTEM="mingw"
+
+# Go in openssl folder
+#cd my-openssl-path
+cd ..
+
+# Clean Build
+make clean
+if [ $? -eq 0 ]; then echo "OK : make clean"; else echo "FAIL : make clean"; exit;  fi
+
+# Configure Build 
+./config --prefix=$(pwd)/builds/mingw no-idea no-mdc2 no-rc5 shared
+if [ $? -eq 0 ]; then echo "OK : ./config"; else echo "FAIL : ./config"; exit;  fi
+
+# Check depend
+make depend
+if [ $? -eq 0 ]; then echo "OK : make depend"; else echo "FAIL : make depend"; exit;  fi
+
+# Build with 8 jobs
+make -j8
+if [ $? -eq 0 ]; then echo "OK : make -j8"; else echo "FAIL : make -j8"; exit;  fi
+
+# Install to prefix
+make install_sw
+if [ $? -eq 0 ]; then echo "OK : make install_sw"; else echo "FAIL : make install_sw"; exit;  fi
+
